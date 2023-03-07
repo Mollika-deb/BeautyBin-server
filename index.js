8a32cbc35293c108c6b82367f369d391968a2726
Mollika-Deb <mollika1551@gmail.com>
2023-03-08 00:34:43 +0600

    update

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run(){
     try{
         const categoryCollection = client.db('beautyBin').collection('category')
         const productCollection = client.db('beautyBin').collection('products')
+        const orderCollection = client.db('beautyBin').collection('order')
 
         app.get('/category', async (req, res)=>{
             const query = {}
@@ -28,20 +29,7 @@ async function run(){
             res.send(categories)
         })
 
-        // app.get('/products', async(req, res)=>{
-        //     let query = {}
-            
-        //     if(req.query.id){
-        //         query = {
-        //             id: req.query.id
-        //         }
-        //     }
-        //     const cursor =  productCollection.find(query);
-        //     const product = await cursor.toArray();
-        //     res.send(product);
-
-        // })
-
+       
         app.get('/products', async (req, res) => {
             
             let query = {};
@@ -53,6 +41,14 @@ async function run(){
             const product = await productCollection.find(query).toArray()
             res.send(product);
          });
+         
+
+         app.post('/order', async(req, res) =>{
+            const orders = req.body;
+            console.log(orders);
+            const result = await orderCollection.insertOne(orders);
+            res.send(result);
+        });
 
     }
     catch{
